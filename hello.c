74cb67461515324c0520dbd8c8a1b8b1ddfb11b6
Joseph McGrath <joseph.mcgrath@new.ox.ac.uk>
2018-05-22 11:50:54 +0100

    Add user input capabilities

--- hello.c
+++ hello.c
@@ -2,7 +2,18 @@
 #include <stdio.h>
 
 int main(void) {
-  printf("Hello, Joe! You are 21 years old.");
+  //Get user name
+  char userName[10];
+  printf("Enter your name: ");
+  scanf("%s", &userName);
+  
+
+  //Get user age
+  int userAge;
+  printf("Enter your age: ");
+  scanf("%d", &userAge);
+    
+  printf("Hello, %s! You are %d years old.\n",userName, userAge);
 
   return(0);
 }
