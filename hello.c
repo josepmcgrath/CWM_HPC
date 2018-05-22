ee8eef3f14fa553b8f4e77da34cd1380bfec2ea6
Joseph McGrath <joseph.mcgrath@new.ox.ac.uk>
2018-05-22 11:28:09 +0100

    Reverted back to displaying age (not squared age)

--- hello.c
+++ hello.c
@@ -1,14 +1,8 @@
 //This program will print "hello,world!" to the screen
 #include <stdio.h>
 
-int square(int number);
-
 int main(void) {
-  int age = 21;
-
-  int square_age = square(age);
-
-  printf("Hello, Joe! The square of your age is  %d.", square_age);
+  printf("Hello, Joe! You are 21 years old.");
 
   return(0);
 }
